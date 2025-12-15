ff75528a495d9d335c91960f4a676c30d1347ace
VANQUYVUONG <vanbin1605@gmail.com>
2025-12-15 14:11:52 +0700

    Create MusicController

--- src/main/java/com/comicapp/comic_api/controller/MusicController.java
+++ src/main/java/com/comicapp/comic_api/controller/MusicController.java
@@ -1,4 +1,41 @@
 package com.comicapp.comic_api.controller;
 
+import com.comicapp.comic_api.dto.request.MusicCreateRequest;
+import com.comicapp.comic_api.dto.response.MusicResponse;
+import com.comicapp.comic_api.service.MusicService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/music")
 public class MusicController {
+
+    private final MusicService musicService;
+
+    public MusicController(MusicService musicService) {
+        this.musicService = musicService;
+    }
+
+    // GET /music
+    @GetMapping
+    public ResponseEntity<List<MusicResponse>> getAllMusic() {
+        return ResponseEntity.ok(musicService.getAllMusic());
+    }
+
+    // POST /music
+    @PostMapping
+    public ResponseEntity<MusicResponse> createMusic(
+            @RequestBody MusicCreateRequest request
+    ) {
+        return ResponseEntity.ok(musicService.createMusic(request));
+    }
+
+    // DELETE /music/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteMusic(@PathVariable Long id) {
+        musicService.deleteMusic(id);
+        return ResponseEntity.ok("Delete music success");
+    }
 }
